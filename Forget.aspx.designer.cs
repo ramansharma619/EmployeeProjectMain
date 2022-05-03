61e0a791377cf8a0cd00cec87430c32ba1ac8422
ramansharma619 <ramasharma@qasource.com>
2022-05-03 10:03:55 +0530

    Minor Additional fixes

--- Forget.aspx.designer.cs
+++ Forget.aspx.designer.cs
@@ -15,6 +15,15 @@ namespace EmployeeProject
     {
 
         /// <summary>
+        /// BackToLogin control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink BackToLogin;
+
+        /// <summary>
         /// alertBox control.
         /// </summary>
         /// <remarks>
